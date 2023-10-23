5d30bd0a1e5a45b4b036a59cbcac946d085ec44d
hih <hih@fb.is>
2023-10-23 14:32:45 +0000

    random mynd

---
+++ oktober_20/js/script.js
@@ -0,0 +1,7 @@
+const characterNameHtml = document.getElementById('characterName');
+
+const characterNames = ['goku', 'kratos', 'laraCroft', 'pikachu'];
+
+const randomNumber = Math.floor(Math.random() * characterNames.length);
+
+characterNameHtml.innerHTML = `<img src="images/${characterNames[randomNumber]}.jpg">` ;
